c7a4888db805ee2658723b310c5c85323e6cf156
Abhinraj-ss <abhinrajss18@gmail.com>
2022-12-06 18:46:39 +0530

    added favicon

--- App.py
+++ App.py
@@ -1,9 +1,17 @@
 from flask import Flask
 from flask_cors import CORS
+import os
+from flask import send_from_directory
 
 app = Flask(__name__)
 CORS(app)
 
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+                               
 @app.route("/",methods=['POST','GET'])
 def hello_world():
     print("printing to console")
