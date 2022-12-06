a16ea1ac80f968b29cf3ca06c290dca8c1040e2b
Abhinraj-ss <abhinrajss18@gmail.com>
2022-12-06 11:45:43 +0530

    flask app initial commit

--- App.py
+++ App.py
@@ -1,1 +1,9 @@
+from flask import Flask
+from flask_cors import CORS
 
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/",methods=['POST'])
+def hello_world():
+    return "<p>Hello, World!</p>",200
